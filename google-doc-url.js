fb69ed5cc24b73897b737a5581e14c4452da1bd2
aajjuarez <albertoandres.juarez@gmail.com>
2023-09-21 17:18:10 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1rP0jFDaPcsb8Xm7dHrVj7ILdJ0AGkULwxGhYPM7qHJE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_bFJwYuQfQiYIu21uGTtMrPGT5gRRJEG5q8husd5_Xs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
